20f09a24c5f14c0dbfdf393cb04b36932b59600e
Xincan <cecilia.fung@foxmail.com>
2022-01-25 12:46:46 +0900

    modify

--- n_gram.py
+++ n_gram.py
@@ -6,7 +6,7 @@ summary:
 
 from collections import Counter
 
-# read train data into sentences
+# read train/test data into sentences
 def read_sentences(path):
     with open(path, encoding='utf-8') as f:
         lines = f.readlines()
@@ -15,7 +15,7 @@ def read_sentences(path):
             sentences.append('<BOS> ' + lines[item].rstrip('.,\n') + '<EOS>')
     return sentences
 
-sentences = read_sentences('wiki-en-train.word')
+train_sentences = read_sentences('wiki-en-train.word')
 
 # make word dictionary
 def make_word_dic(sentences):
@@ -27,7 +27,7 @@ def make_word_dic(sentences):
             word_dic[word] = word_dic.get(word, 0) + 1
     return word_dic
 
-word_dic = make_word_dic(sentences)
+word_dic = make_word_dic(train_sentences)
 
 '''
 construct n-gram corpus:
@@ -35,7 +35,7 @@ read each sentence,
 extract all n-gram lists, and all (n-1)-gram lists in each sentence,
 count the number of all n-gram lists and all (n-1)-gram lists,
 '''
-def construct_ngram_corpus(n, sentences):
+def construct_ngram_corpus(sentences, n=3):
     ngram_list = [] # n grams
     prefix_list = [] # n-1 grams
     for i, item in enumerate(sentences):
@@ -48,8 +48,19 @@ def construct_ngram_corpus(n, sentences):
     prefix_counter = Counter(prefix_list)
     return ngram_counter, prefix_counter
 
-ngram_counter, prefix_counter = construct_ngram_corpus(3, sentences)
+ngram_counter, prefix_counter = construct_ngram_corpus(train_sentences)
 
-# hyperparameter of knears smoothing
-d = 0.75
-lam = 0.05
+# calculate the probability of the sentence
+def calculate_sentence_probability(sentences, n=3):
+    probability_list = []
+    probability = 1
+    for i, item in enumerate(sentences):
+        ngram = list(zip(*[item.split()[i:] for i in range(n)]))
+        for item in ngram:
+            probability *= ngram_counter[item] / (prefix_counter[(item[0], item[1])] + 1)
+            probability_list.append(probability)
+    return probability_list
+
+test_sentences = read_sentences('wiki-en-test.word')
+print(calculate_sentence_probability(test_sentences))
+
