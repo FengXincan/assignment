5e9bce2fe3ca3e98594b40d71fa1e8bd154ae64c
Xincan <cecilia.fung@foxmail.com>
2022-02-08 13:22:37 +0900

    02

--- 1_Sequence_Labelling_and_Parsing/hmm.py
+++ 1_Sequence_Labelling_and_Parsing/hmm.py
@@ -1,6 +1,14 @@
 # training part
 # read training data
+def read_sentences(path):
+    with open(path, encoding='utf-8') as f:
+        lines = f.readlines()
+        sentences = []
+        for item in range(len(lines)):
+            sentences.append(lines[item].rstrip('.,\n'))
+    return sentences
 
+train_sentences = read_sentences('wiki-en-train.norm_pos')
 # clean data, set the unknown token as 'UNK'
 # do train
 
